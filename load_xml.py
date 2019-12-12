cb942cb10e140195461e1baa18bdda80fd90e8a9
Matthew Dodd <matthew.dodd@sunlife.com>
2019-12-12 13:10:17 -0500

    small change to commenting

--- load_xml.py
+++ load_xml.py
@@ -6,24 +6,22 @@ sesdir = os.fsencode('./OBO_XML_72/sessionsPapers')
 #oadir = os.fsencode('./OBO_XML_72/ordinarysAccounts')
 #rows = ['div0','div1','interp','join','persName','rs','xptr']
 
-"""
-try:
-  cnx = mysql.connector.connect(host="localhost",
-                                database='employees',
-                                user="root",
-                                passwd="<change_this>")
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cnx.close()
-
-cursor = cnx.cursor()
-"""
+#try:
+#  cnx = mysql.connector.connect(host="localhost",
+#                                database='employees',
+#                                user="root",
+#                                passwd="<change_this>")
+#except mysql.connector.Error as err:
+#  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#    print("Something is wrong with your user name or password")
+#  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#    print("Database does not exist")
+#  else:
+#    print(err)
+#else:
+#  cnx.close()
+#
+#cursor = cnx.cursor()
 
 for filename in os.listdir(sesdir):
     if filename.endswith(".xml"): 
@@ -36,4 +34,4 @@ for filename in os.listdir(sesdir):
     else:
         continue
 
-cnx.close
+#cnx.close

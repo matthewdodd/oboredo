ba9f8e07ac7a37bde23bd01a6889363f3fad545d
Matthew Dodd <matthew.a.dodd@gmail.com>
2019-12-14 21:27:41 -0500

    set up mysql connection:

--- load_xml.py
+++ load_xml.py
@@ -2,29 +2,25 @@
 #tutorial where this began: https://www.datacamp.com/community/tutorials/python-xml-elementtree
 
 import xml.etree.ElementTree as ET
-import mysql, glob, os
+import mysql.connector as mysql
+import glob, os
 
 parent_dir = 'C:/projects/oboredo/OBO_XML_72/sessionsPapers'
 #oadir = os.fsencode('C:/projects/oboredo/OBO_XML_72/ordinarysAccounts')
 #rows = ['div0','div1','interp','join','persName','rs','xptr']
 elements = []
 
-#try:
-#  cnx = mysql.connector.connect(host="localhost",
-#                                database='employees',
-#                                user="root",
-#                                passwd="<change_this>")
-#except mysql.connector.Error as err:
-#  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-#    print("Something is wrong with your user name or password")
-#  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-#    print("Database does not exist")
-#  else:
-#    print(err)
-#else:
-#  cnx.close()
-#
-#cursor = cnx.cursor()
+try:
+  cnx = mysql.connect(host="localhost",
+                        database="oboredo",
+                        user="root",
+                        passwd="<change_this>",
+                        port="3306",
+                        auth_plugin='mysql_native_password')
+except mysql.Error as err:
+  print(err)
+
+cursor = cnx.cursor()
 
 for xml_file in glob.glob(os.path.join(parent_dir, '*.xml')):
   print (xml_file)
@@ -36,17 +32,22 @@ for xml_file in glob.glob(os.path.join(parent_dir, '*.xml')):
 #    print(elements)
   for rec in root.iter('div0'):
     print("div0 attribute is",str(rec.attrib))
-  for rec in root.iter('div1'):
-    print("div1 attribute is",str(rec.attrib))
-  for rec in root.iter('interp'):
-    print("interp attribute is",str(rec.attrib))
-  for rec in root.iter('xptr'):
-    print("xptr attribute is",str(rec.attrib))
-  for rec in root.iter('join'):
-    print("join attribute is",str(rec.attrib))
-  for rec in root.iter('persName'):
-    print("persName attribute is",str(rec.attrib))
-  for rec in root.iter('rs'):
-    print("rs attribute is",str(rec.attrib))
+    div0_sql = "insert into oboredo.raw_div0(type, id) values (%s, %s)"
+    div0_val = (str(rec.attrib.get("type")), str(rec.attrib.get("id")))
+    #print (rec.attrib.get("type"))
+    cursor.execute(div0_sql, div0_val)
+    cnx.commit()
+  #for rec in root.iter('div1'):
+  #  print("div1 attribute is",str(rec.attrib))
+  #for rec in root.iter('interp'):
+  #  print("interp attribute is",str(rec.attrib))
+  #for rec in root.iter('xptr'):
+  #  print("xptr attribute is",str(rec.attrib))
+  #for rec in root.iter('join'):
+  #  print("join attribute is",str(rec.attrib))
+  #for rec in root.iter('persName'):
+  #  print("persName attribute is",str(rec.attrib))
+  #for rec in root.iter('rs'):
+  #  print("rs attribute is",str(rec.attrib))
 
 #cnx.close

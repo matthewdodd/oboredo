ef885a171f4516bbc2f53417c2ea56ab2701f28a
Matthew Dodd <matthew.dodd@sunlife.com>
2019-12-18 10:18:17 -0500

    adding in multithreading

--- load_xml.py
+++ load_xml.py
@@ -10,6 +10,7 @@
 
 import xml.etree.ElementTree as ET
 import mysql.connector as mysql
+import concurrent.futures
 import glob, os, datetime, logging
 
 logging.basicConfig(level=logging.DEBUG, filename='load_xml.log', format='%(name)s - %(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
@@ -18,6 +19,7 @@ a = datetime.datetime.now()
 logging.info('Start time is %s', a)
 
 dirs = ['C:/projects/oboredo/OBO_XML_72/sessionsPapers', 'C:/projects/oboredo/OBO_XML_72/ordinarysAccounts']
+tag = ['div0', 'div1', 'interp', 'xptr', 'join', 'persName', 'rs']
 files = []
 div0 = []
 div1 = []
@@ -35,56 +37,67 @@ join_sql = "insert into oboredo.raw_join(result, id, targOrder, targets) values 
 persName_sql = "insert into oboredo.raw_persName(id, type) values (%s, %s)"
 rs_sql = "insert into oboredo.raw_rs(id, type) values (%s, %s)"
 
-for parent_dir in dirs:
-  for xml_file in glob.glob(os.path.join(parent_dir, '*.xml')):
-    files.append(xml_file)
-
-with files as xf:
+def gather_info(xf):
     logging.info('XML file being interpreted is %s', xf)
     tree = ET.parse(xf)
     root = tree.getroot()
     for rec in root.iter('div0'):
-      div0.append(str(rec.attrib.get("type")), str(rec.attrib.get("id")))
+        div0.append(str(rec.attrib.get("type")), str(rec.attrib.get("id")))
     for rec in root.iter('div1'):
-      div1.append(str(rec.attrib.get("type")), str(rec.attrib.get("id")))
+        div1.append(str(rec.attrib.get("type")), str(rec.attrib.get("id")))
     for rec in root.iter('interp'):
-      interp.append(str(rec.attrib.get("inst")), str(rec.attrib.get("type")), str(rec.attrib.get("value")))
+        interp.append(str(rec.attrib.get("inst")), str(rec.attrib.get("type")), str(rec.attrib.get("value")))
     for rec in root.iter('xptr'):
-      xptr.append(str(rec.attrib.get("type")), str(rec.attrib.get("doc")))
+        xptr.append(str(rec.attrib.get("type")), str(rec.attrib.get("doc")))
     #join is going to be different, some join xml tags don't have everything so we will input default values
     for rec in root.iter('join'):
-      join.append(str(rec.attrib.get("result", "unknown")), str(rec.attrib.get("id", "unknown")), str(rec.attrib.get("targOrder", "M")), str(rec.attrib.get("targets", "unknown")))
+        join.append(str(rec.attrib.get("result", "unknown")), str(rec.attrib.get("id", "unknown")), str(rec.attrib.get("targOrder", "M")), str(rec.attrib.get("targets", "unknown")))
     for rec in root.iter('persName'):
-      persName.append(str(rec.attrib.get("id")), str(rec.attrib.get("type")))
+        persName.append(str(rec.attrib.get("id")), str(rec.attrib.get("type")))
     for rec in root.iter('rs'):
-      rs.append(str(rec.attrib.get("id")), str(rec.attrib.get("type")))
+        rs.append(str(rec.attrib.get("id")), str(rec.attrib.get("type")))
+
+def run_sql(tags):
+    if (tags == 'div0'):
+        cursor.executemany(div0_sql, div0)
+    elif (tags == 'div1'):
+        cursor.executemany(div1_sql, div1)
+    elif (tags == 'interp'):
+        cursor.executemany(interp_sql, interp)
+    elif (tags == 'xptr'):
+        cursor.executemany(xptr_sql, xptr)
+    elif (tags == 'join'):
+        cursor.executemany(join_sql, join
+    elif (tags == 'persName'):
+        cursor.executemany(persName_sql, persName)
+    elif (tags == 'rs'):
+        cursor.executemany(rs_sql, rs)
+
+for parent_dir in dirs:
+    for xml_file in glob.glob(os.path.join(parent_dir, '*.xml')):
+        files.append(xml_file)
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(gather_info, files)
 
 try:
-  cnx = mysql.connect(host="localhost",
+    cnx = mysql.connect(host="localhost",
                         database="oboredo",
                         user="root",
                         passwd="<change this>",
                         port="3306",
                         auth_plugin='mysql_native_password')
 except mysql.Error as err:
-  logging.error(f'MySQL error raised: %s', err)
+    logging.error(f'MySQL error raised: %s', err)
 
 cursor = cnx.cursor()
 cursor.execute('SET autocommit = 0')
 cnx.commit()
 
-try:
-  cursor.executemany(div0_sql, div0)
-  cursor.executemany(div1_sql, div1)
-  cursor.executemany(interp_sql, interp)
-  cursor.executemany(xptr_sql, xptr)
-  cursor.executemany(join_sql, join)
-  cursor.executemany(persName_sql, persName)
-  cursor.executemany(rs_sql, rs)
-  cnx.commit
-except mysql.Error as err:
-  logging.error(f'MySQL error raised: %s', err)
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(run_sql, tag)
 
+cnx.commit
 cnx.close
 logging.warning('cnx is now closed')
 
